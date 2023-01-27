d774c0133cd342d12810612c7e1d804275e18636
isaeme23 <isabella.manrique@mail.escuelaing.edu.co>
2023-01-27 18:24:39 -0500

    Adelanto segundo punto, getDigitsPi sin probar pero intenta generar threads impares para resolver los numeros de Pi

--- src/main/java/edu/eci/arsw/math/PiDigits.java
+++ src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -12,9 +12,121 @@ public class PiDigits {
 
     public static int maxValue = 10;
 
+    private static int DigitsPerSum = 8;
+    private static double Epsilon = 1e-17;
+
     public static void main(String[] args) {
-        PiThread threadP = new PiThread();
-        threadP.setAB(2, 1);
-        threadP.start();
+        getDigitsPi(500, 2);
+    }
+
+    public static void getDigitsPi(int amount, int N){
+        int value = (amount/N);
+        int start= 0;
+        for (int i = 0; i < N; i ++) {
+            PiThread threadP = new PiThread();
+            threadP.setAB(start, value);
+            start += value;
+            threadP.start();
+        }
+    }
+
+
+    public static byte[] getDigits(int start, int count) {
+        if (start < 0) {
+            throw new RuntimeException("Invalid Interval");
+        }
+
+        if (count < 0) {
+            throw new RuntimeException("Invalid Interval");
+        }
+
+        byte[] digits = new byte[count];
+        double sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (i % DigitsPerSum == 0) {
+                sum = 4 * sum(1, start)
+                        - 2 * sum(4, start)
+                        - sum(5, start)
+                        - sum(6, start);
+
+                start += DigitsPerSum;
+            }
+
+            sum = 16 * (sum - Math.floor(sum));
+            digits[i] = (byte) sum;
+        }
+
+        bytesToHex(digits);
+        return digits;
+    }
+
+    private static double sum(int m, int n) {
+        double sum = 0;
+        int d = m;
+        int power = n;
+
+        while (true) {
+            double term;
+
+            if (power > 0) {
+                term = (double) hexExponentModulo(power, d) / d;
+            } else {
+                term = Math.pow(16, power) / d;
+                if (term < Epsilon) {
+                    break;
+                }
+            }
+
+            sum += term;
+            power--;
+            d += 8;
+        }
+
+        return sum;
+    }
+
+    private static int hexExponentModulo(int p, int m) {
+        int power = 1;
+        while (power * 2 <= p) {
+            power *= 2;
+        }
+
+        int result = 1;
+
+        while (power > 0) {
+            if (p >= power) {
+                result *= 16;
+                result %= m;
+                p -= power;
+            }
+
+            power /= 2;
+
+            if (power > 0) {
+                result *= result;
+                result %= m;
+            }
+        }
+
+        return result;
+    }
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<hexChars.length;i=i+2){
+            //sb.append(hexChars[i]);
+            sb.append(hexChars[i+1]);
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 }
